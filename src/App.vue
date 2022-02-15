30fdbd4c6bce90a8b18b6015ac2fadbada1cd1a7
Eric Diego Oliveira do Nascimento <88443797+EricOliveiras@users.noreply.github.com>
2022-02-15 11:48:57 -0300

    learning vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <Header />
     <PrimeiroComponente />
     <LifeCycle />
     <Pessoa />
@@ -7,6 +8,7 @@
 </template>
 
 <script>
+  import Header from './components/Header.vue'
   import PrimeiroComponente from './components/PrimeiroComponente.vue'
   import LifeCycle from './components/LifeCycle.vue'
   import Pessoa from './components/Pessoa.vue'
@@ -16,7 +18,8 @@
     components: {
       PrimeiroComponente,
       LifeCycle,
-      Pessoa
+      Pessoa,
+      Header
     }
   }
 </script>
--- src/components/Info.vue
+++ src/components/Info.vue
@@ -2,12 +2,21 @@
   <div>
     <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
     <p v-else>Não estou trabalhando no momento.</p>
-    <p>Utilizo as seguintes tecnologias:</p>
+    <p>Utilizo as seguintes tecnologias BackEnd:</p>
     <ul>
-      <li>JavaScript</li>
-      <li>Node.JS</li>
-      <li>Sequelize ORM</li>
+      <li v-for="(technolgy, index) in technolgies" v-bind:key="index">
+        {{ technolgy }}
+      </li>
     </ul>
+    <p>Utilizo as seguintes tecnologias FrontEnd:</p>
+    <ul>
+      <li v-for="technolgy in tech_front" :key="technolgy.id">
+        {{ technolgy.language }}
+      </li>
+    </ul>
+    <div>
+      <button @click="showEmail">{{ texto_botao }}</button>
+    </div>
     <p v-show="mostrar_email">Enviar email para: {{ email }}</p>
     <p>Para acessar meu portifóolio basta clicar <a v-bind:href="meu_link" target="_blank">aqui</a></p>
     <Picture />
@@ -22,13 +31,34 @@ export default {
   data() {
     return {
       esta_trabalhando: false,
-      mostrar_email: true,
+      mostrar_email: false,
       email: 'eric@email.com',
-      meu_link: 'http://www.google.com'
+      meu_link: 'http://www.google.com',
+      texto_botao: 'Mostrar Email',
+      technolgies: [
+        'JavaScript',
+        'Node.JS',
+        'Sequelize ORM'
+      ],
+      tech_front: [
+        {id: 1, language: 'Html'},
+        {id: 2, language: 'CSS'},
+        {id: 3, language: 'Vue'}
+      ]
     }
   },
   components: {
     Picture
+  },
+  methods: {
+    showEmail() {
+      this.mostrar_email = !this.mostrar_email
+      if (!this.mostrar_email) {
+          this.texto_botao = 'Mostrar email'
+        } else {
+          this.texto_botao = 'Esconder email'
+      }
+    }
   }
 }
 </script>
